530e0320c4d1417ba9d47cee18fa1f6e32aeaec3
Abdoulie Jallow <jallow.jku@gmail.com>
2024-10-12 15:47:08 +0200

    lab session: notes complete

--- chapter2.R
+++ chapter2.R
@@ -1,3 +1,5 @@
+install.packages("car")
+library(car)
 library(MASS)
 library(ISLR2)
 
@@ -31,4 +33,49 @@ plot(hatvalues(lm.fit)) # leverage statistics can be computed for any number of 
 which.max(hatvalues(lm.fit)) # identifies the index of the largest element of a vector
 
 # Multiple linear Regression
+lm.fit <- lm(medv ~ lstat + age, data = Boston)
+summary(lm.fit)
+
+lm.fit <- lm(medv ~ ., data = Boston) # Regression for all the 12 variables of the Boston data set. Typing all the variable will be cumbersome hence we use the dot (.) short-hand
+summary(lm.fit)
+
+?summary.lm # we can access the individual components of a summary object by name.
+summary(lm.fit)$fstatistic
+
+vif(lm.fit) # Function to calculate the variance inflation factors. to check for collinearity
+
+lm.fit1 <- lm(medv ~ . -age, data = Boston) # Perform a regression using all the variables but one. for example, remove age since it has a high p-value.
+summary(lm.fit1)
+lm.fit1 <- update(lm.fit, ~. -age) # An alternative method is to use the update() function
+
+# Interaction Terms
+summary(lm(medv ~ lstat * age, data = Boston))
+
+# Non-linear Transformations of the predictors
+lm.fit2 <- lm(medv ~ lstat + I(lstat^2)) # Since "^" have a special meaning in a formula object, the I() is used a wraper
+summary(lm.fit2)
+
+lm.fit <- lm(medv ~ lstat)
+anova(lm.fit, lm.fit2) # The near zero p-value associated with the quadratic term suggests that it leads to an improved model. The anova() further quantify the extent of to which the quadratic fit is superior to the linear fit.
+
+par(mfrow = c(2, 2))
+plot(lm.fit2)
+
+lm.fit5 <- lm(medv ~ poly(lstat, 5)) # A better approach involves using the poly() function to create the polynomial within lm().
+summary(lm.fit5)
+
+summary(lm(medv ~ log(rm), data = Boston)) # log transformation of the predictor can also be used instead of polynomial transformations
+
+# Qualitative Predictors
+head(Carseats)
+attach(Carseats)
+
+lm.fit <- lm(Sales ~ . + Income:Advertising + Price:Age,
+             data = Carseats) # R generates dummy variables automatically. Here is a multiple regression model that includes some interaction terms.
+summary(lm.fit)
+
+contrasts(ShelveLoc) # Function to return the coding that R uses for the dummy variables
+?contrasts
+
+#----- Exercises ----
 
